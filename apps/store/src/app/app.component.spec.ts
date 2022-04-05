58659f8437b8e5d88f4edd579e10f7326c121119
Caleb Ukle <caleb@nrwl.io>
2022-04-05 12:20:19 -0500

    fix tests

--- apps/store/src/app/app.component.spec.ts
+++ apps/store/src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { StoreFeatureGameDetailModule } from '@bg-hoard/store/feature-game-detail';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
+import { MatCardModule } from '@angular/material/card';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        StoreUiSharedModule,
+        StoreFeatureGameDetailModule,
+        RouterTestingModule,
+        MatCardModule,
+        HttpClientTestingModule,
+      ],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -16,18 +26,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'store'`, () => {
+  it(`should have as title 'Board Game Hoard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('store');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome to store!'
-    );
+    expect(app.title).toEqual('Board Game Hoard');
   });
 });
